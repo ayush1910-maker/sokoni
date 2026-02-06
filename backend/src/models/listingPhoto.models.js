6687b4374e1870f16042b085305da8313c813d46
Ayush Porwal <pa9067628@gmail.com>
2026-02-06 15:40:33 +0530

    listingPhoto model for store photos seprately

---
+++ backend/src/models/listingPhoto.models.js
@@ -0,0 +1,32 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../db/db.js";
+
+const ListingPhoto = sequelize.define("listing_photos", 
+    {
+        id: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        listing_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: "listing",
+                key: "id"
+            },
+            onDelete: "CASCADE"
+        },
+        image_url: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    },
+    {
+        tableName: "listing_photos",
+        timestamps: true
+    }
+)
+
+export default ListingPhoto
