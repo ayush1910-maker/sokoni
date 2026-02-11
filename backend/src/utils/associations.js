22ca3ddda6067c8d67f191120b74962c65e4c6d7
Ayush Porwal <pa9067628@gmail.com>
2026-02-11 17:35:35 +0530

    setup assocciation for adding multiple staff

--- backend/src/utils/associations.js
+++ backend/src/utils/associations.js
@@ -1,32 +1,69 @@
-import Listing from "../models/listing.models.js";
+import Listing from "../models/listing.models.js"
 import ListingPhoto from "../models/listingPhoto.models.js"
-import Staff from "../models/staff.models.js";
-import User from "../models/user.models.js";
-import Category from "../models/category.models.js";
-import SubCategory from "../models/subCategory.models.js";
+import Staff from "../models/staff.models.js"
+import User from "../models/user.models.js"
+import Category from "../models/category.models.js"
+import SubCategory from "../models/subCategory.models.js"
+import ListingStaff from "../models/listingStaff.models.js"
 
-User.hasMany(Listing , {
-    foreignKey: "user_id"
+
+User.hasMany(Listing, {
+    foreignKey: "user_id",
+    as: "listings"
 })
 
-Listing.belongsTo(ListingPhoto , {
-    foreignKey: "user_id"
+Listing.belongsTo(User, {
+    foreignKey: "user_id",
+    as: "user"
 })
-
 
 Listing.hasMany(ListingPhoto, {
-    foreignKey: "listing_id"
+    foreignKey: "listing_id",
+    as: "photos"
 })
 
-ListingPhoto.belongsTo(Listing , {
-    foreignKey: "listing_id"
+ListingPhoto.belongsTo(Listing, {
+    foreignKey: "listing_id",
+    as: "listing"
 })
 
 
-User.hasMany(Staff , {
-    foreignKey: "business_id"
+User.hasMany(Staff, {
+    foreignKey: "business_id",
+    as: "staff"
 })
 
-Staff.belongsTo(User , {
-    foreignKey: "business_id"
+Staff.belongsTo(User, {
+    foreignKey: "business_id",
+    as: "business"
 })
+
+
+Category.hasMany(SubCategory, {
+    foreignKey: "category_id",
+    as: "subCategories"
+})
+
+SubCategory.belongsTo(Category, {
+    foreignKey: "category_id",
+    as: "category"
+})
+
+Listing.belongsToMany(Staff, {
+  through: "listing_staff",
+  foreignKey: "listing_id"
+});
+
+Staff.belongsToMany(Listing, {
+  through: "listing_staff",
+  foreignKey: "staff_id"
+});
+
+export {
+    User,
+    Staff,
+    Listing,
+    ListingPhoto,
+    Category,
+    SubCategory
+}
